7b9d73dc3ada008924ddbf1f7e7f4524f15fe2cd
Matthew Okalebo <mokalebo@unomaha.edu>
2020-02-01 17:55:26 -0600

    Third Commit

--- clientms2/clientms2/settings.py
+++ clientms2/clientms2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tl+#@7c6@#yub)m$-2v5sh@6sp@wmstigo&ljp@#ov$0orsnl*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
